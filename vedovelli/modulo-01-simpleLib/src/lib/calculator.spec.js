acdf945e407ea5124b89bba25509fd68d947f003
Josué Heringer <josuedelgadoheringer98@gmail.com>
2022-08-23 23:49:08 -0300

    Finalizando aula 13 e formatando para receber outros modulos

---
+++ vedovelli/modulo-01-simpleLib/src/lib/calculator.spec.js
@@ -0,0 +1,20 @@
+import { sum } from "./calculator";
+
+it("sum 2 and 2 and the result must be 4", async () => {
+  expect(sum(2, 2)).toBe(4);
+});
+it("shold sum 2 and 2 even if one of them is a string and the result must be 4", async () => {
+  expect(sum("2", "2")).toBe(4);
+});
+
+it("should  throw an error if what is provided to the method", () => {
+  expect(() => {
+    sum("", "2");
+  }).toThrowError();
+  expect(() => {
+    sum(["2", "2"]);
+  }).toThrowError();
+  expect(() => {
+    sum({});
+  }).toThrowError();
+});
