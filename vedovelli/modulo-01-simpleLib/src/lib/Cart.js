c8b68a535ed43252a65e7c57a8c9553567ec468e
Josué Heringer <josuedelgadoheringer98@gmail.com>
2022-09-05 19:40:27 -0300

    Finalizando CHeckout do carrinho

---
+++ vedovelli/modulo-01-simpleLib/src/lib/Cart.js
@@ -0,0 +1,42 @@
+import find from "lodash/find";
+import remove from "lodash/remove";
+
+export default class Cart {
+  items = [];
+
+  add(item) {
+    const itemToFind = { product: item.product };
+    if (find(this.items, itemToFind)) {
+      remove(this.items, itemToFind);
+    }
+    this.items.push(item);
+  }
+
+  remove(product) {
+    remove(this.items, { product });
+  }
+
+  getTotal() {
+    return this.items.reduce((acc, item) => {
+      return acc + item.quantity * item.product.price;
+    }, 0);
+  }
+
+  summary() {
+    const total = this.getTotal();
+    const items = this.items;
+    return {
+      total,
+      items,
+    };
+  }
+  checkout() {
+    const total = this.getTotal();
+    const items = this.items;
+    this.items = [];
+    return {
+      total,
+      items,
+    };
+  }
+}
---
+++ vedovelli/modulo-01-simpleLib/src/lib/Cart.spec.js
@@ -0,0 +1,100 @@
+import Cart from "./Cart";
+
+describe("Card", () => {
+  let cart;
+  let product = {
+    title: "Adidas running shoes - men",
+    price: 35388, // 353.88 | R$383,88
+  };
+  let product2 = {
+    title: "Adidas running shoes - women",
+    price: 41872, // 353.88 | R$383,88
+  };
+  beforeEach(() => {
+    cart = new Cart();
+  });
+  describe("getTotal()", () => {
+    it("should return 0 when getTotal is executed in a newly created instance", async () => {
+      expect(cart.getTotal()).toEqual(0);
+    });
+    it("should multiply quantity and price receive the total amount", () => {
+      const item = {
+        product,
+        quantity: 2, // 70776
+      };
+
+      cart.add(item);
+
+      expect(cart.getTotal()).toEqual(70776);
+    });
+    it("should ensure no more than on product exists at a time", () => {
+      cart.add({
+        product,
+        quantity: 2, // 70776
+      });
+
+      cart.add({
+        product,
+        quantity: 1,
+      });
+
+      expect(cart.getTotal()).toEqual(35388);
+    });
+    it("should update total when a product gets included and then removed", () => {
+      cart.add({
+        product,
+        quantity: 2, // 70776
+      });
+
+      cart.add({
+        product: product2,
+        quantity: 1,
+      });
+
+      cart.remove(product);
+
+      expect(cart.getTotal()).toEqual(41872);
+    });
+  });
+
+  describe("checkout()", () => {
+    it("should return an object with the total and the list of items", () => {
+      cart.add({
+        product,
+        quantity: 2,
+      });
+
+      cart.add({
+        product: product2,
+        quantity: 3,
+      });
+      expect(cart.checkout()).toMatchSnapshot();
+    });
+    it("should return an object with the total and the list of items when summary() is called", () => {
+      cart.add({
+        product,
+        quantity: 2,
+      });
+
+      cart.add({
+        product: product2,
+        quantity: 3,
+      });
+      expect(cart.summary()).toMatchSnapshot();
+      expect(cart.getTotal()).toBeGreaterThan(0);
+    });
+    it("should reset the cart when checkout() is called", () => {
+      cart.add({
+        product,
+        quantity: 2,
+      });
+
+      cart.add({
+        product: product2,
+        quantity: 3,
+      });
+      cart.checkout();
+      expect(cart.getTotal()).toEqual(0);
+    });
+  });
+});
