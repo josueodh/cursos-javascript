f84552797e38ea0d2b002580fef9ee6591bc58bb
Josué Heringer <josuedelgadoheringer98@gmail.com>
2022-09-05 22:46:19 -0300

    Finalizando modulo 01

--- vedovelli/modulo-01-simpleLib/src/lib/Cart.js
+++ vedovelli/modulo-01-simpleLib/src/lib/Cart.js
@@ -1,44 +1,8 @@
 import Dinero from "dinero.js";
 import find from "lodash/find";
 import remove from "lodash/remove";
+import { calculateDiscount } from "./discount.utils";
 
-const calculatePercentageDiscount = (amount, { condition, quantity }) => {
-  if (condition?.percentage && quantity > condition.minimum) {
-    return amount.percentage(condition.percentage);
-  }
-
-  return Money({ amount: 0 });
-};
-
-const calculateQuantityDiscount = (amount, { condition, quantity }) => {
-  const isEven = quantity % 2 === 0;
-
-  if (condition?.quantity && quantity > condition.quantity) {
-    return amount.percentage(isEven ? 50 : 40);
-  }
-  return Money({ amount: 0 });
-};
-
-const calculateDiscount = (amount, quantity, condition) => {
-  const list = Array.isArray(condition) ? condition : [condition];
-
-  const [higherDiscount] = list
-    .map(c => {
-      if (c.percentage) {
-        return calculatePercentageDiscount(amount, {
-          condition: c,
-          quantity,
-        }).getAmount();
-      } else if (c.quantity) {
-        return calculateQuantityDiscount(amount, {
-          condition: c,
-          quantity,
-        }).getAmount();
-      }
-    })
-    .sort((a, b) => b - a);
-  return Money({ amount: higherDiscount });
-};
 const Money = Dinero;
 
 Money.defaultCurrency = "BRL";
