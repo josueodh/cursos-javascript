4087f6034b2826b9a33da89b5f93055293f7927c
Josué Heringer <josuedelgadoheringer98@gmail.com>
2022-10-17 20:26:24 -0300

    Finalizando Input

--- rocketseat/react-native/ignite_2021/gofinances/App.tsx
+++ rocketseat/react-native/ignite_2021/gofinances/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
 import theme from "./src/global/styles/theme";
-import { Dashboard } from "./src/screens/Dashboard";
+import { Register } from "./src/screens/Register";
 
 import {
   useFonts,
@@ -19,7 +19,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard />
+      <Register />
     </ThemeProvider>
   );
 }
